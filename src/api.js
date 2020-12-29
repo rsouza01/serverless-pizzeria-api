c3795af262ac3a64b038a12e3754ec5774b1e3ce
Rodrigo de Souza <rsouza01@gmail.com>
2020-12-29 22:44:57 +0000

    Api entrypoint

---
+++ src/api.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const Api = require('claudia-api-builder');
+
+const api = new Api();
+
+api.get('/pizzas', () => {
+    return [
+        'Capricciosa',
+        'Quattro Formaggi',
+        'Napoletana',
+        'Margherita'
+    ]
+});
+
+module.exports = api;
