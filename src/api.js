38dd3cc30d6fcdd8dfe9f2f1f098c1a916b3b44c
Rodrigo de Souza <rsouza01@gmail.com>
2021-01-01 23:21:13 +0000

    Cognito auth flow

--- src/api.js
+++ src/api.js
@@ -12,6 +12,10 @@ const deleteOrder = require('./handlers/delete-order')
 
 const updateDeliveryStatus = require('./handlers/update-delivery-status')
 
+api.registerAuthorizer('userAuthentication', {
+    providerARNs: [process.env.userPoolArn]
+    })
+
 api.get('/', () => 'Welcome to Pizza API')
 
 api.get('/pizzas', () => {
@@ -27,33 +31,41 @@ api.post('/orders', (request) => {
     return createOrder(request.body)
 }, {
     success: 201,
-    error: 400
+    error: 400,
+    cognitoAuthorizer: 'userAuthentication'
 })
 
 api.get('/orders', () => {
     return getOrders()
+}, {
+    error: 404,
+    cognitoAuthorizer: 'userAuthentication'
 })
 
 api.get('/orders/{id}', (request) => {
     return getOrders(request.pathParams.id)
 }, {
-    error: 404
+    error: 404,
+    cognitoAuthorizer: 'userAuthentication'
 })
 
 api.put('/orders/{id}', (request) => {
     return updateOrder(request.pathParams.id, request.body)
 }, {
-    error: 400
+    error: 400,
+    cognitoAuthorizer: 'userAuthentication'
 })
 
 api.delete('/orders/{id}', (request) => deleteOrder(request.pathParams.id), {
     success: 200,
-    error: 400
+    error: 400,
+    cognitoAuthorizer: 'userAuthentication'
 })
 
 api.post('/delivery', (request) => updateDeliveryStatus(request.body), {
     success: 200,
-    error: 400
+    error: 400,
+    cognitoAuthorizer: 'userAuthentication'
 })
 
 
--- src/handlers/create-order.js
+++ src/handlers/create-order.js
@@ -10,15 +10,22 @@ const tableName = `${process.env.APP_NAME}-orders-${process.env.NODE_ENV}`
 
 module.exports = function createOrder(request) {
     console.log('Save an order =>', request);
-    
+    const userData = request.context.authorizer.claims
+    console.log('User data', userData)
+
     if (!request || !request.pizza || !request.address) {
         throw new Error('To order pizza please provide pizza type and address where pizza should be delivered')
+    }
+
+    let userAddress = request.body && request.body.address
+    if (!userAddress) {
+        userAddress = JSON.parse(userData.address).formatted
     }
 
     const requestDeliveryBody = {
         pickupTime: '15.34pm',
         pickupAddress: 'Aunt Maria Pizzeria',
-        deliveryAddress: request.address,
+        deliveryAddress: userAddress,
         webhookUrl: `${process.env.BASE_URL}/delivery`,
     }
 
@@ -34,9 +41,10 @@ module.exports = function createOrder(request) {
             return docClient.put({
                 TableName: tableName,
                 Item: {
+                    cognitoUsername: userAddress['cognito:username'],
                     orderId: response.deliveryId,
                     pizza: request.pizza,
-                    address: request.address,
+                    address: userAddress,
                     orderStatus: 'pending'
                 }
             }).promise()
